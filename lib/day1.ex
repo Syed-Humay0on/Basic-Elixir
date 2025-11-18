82658d8a9ee097655dfcf446823a49ea9645f698
Proto <Humayoon04syed@gmail.com>
2025-11-18 18:48:18 +0500

    pattern matching datatypes

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,8 +1,19 @@
-defmodule day01 do
+defmodule Day1 do
  def greet(:male, name), do: "Mr. #{name}"
  def greet(:female, name), do: "Mrs. #{name}"
  def greet(_, name), do: name 
 
-
-
+#DataType Demonstration
+def data_types do
+IO.puts("Integer: #{42}")
+IO.puts("Float: #{3.14}")
+IO.puts("String: #{"Elixir"}")
+IO.puts("Atom: #{:ok}")
+IO.puts("List: #{inspect([1,2,3])}")
+IO.puts("Tuple: #{inspect ({:ok, "data"})}")
+IO.puts("Map: #{inspect(%{key: "value"})}")
 end
+end
+# Run with: elixir lib/day1.ex
+Day1.data_types()
+IO.puts(Day1.greet(:male, "Alex"))
