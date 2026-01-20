fbfdb2ed3e4cad4b18cc19883c342c83bf752a50
Proto <Humayoon04syed@gmail.com>
2026-01-20 22:58:33 +0500

    recursions

--- lib/recursion.ex
+++ lib/recursion.ex
@@ -1,7 +1,6 @@
-# Check API
 defmodule Math do
   def sum_list([head | tail], accumulator) do
-    sum_list(tail, head + accumulator)
+    sum_list(tail, head * accumulator)
   end
 
   def sum_list([], accumulator) do
