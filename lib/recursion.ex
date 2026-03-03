beb7c48153c383ef47e6ac528a3acbf97d1c7344
Proto <Humayoon04syed@gmail.com>
2026-03-03 15:42:24 +0500

    💧ELixir: [Recursions magic]

--- lib/recursion.ex
+++ lib/recursion.ex
@@ -6,6 +6,15 @@ defmodule Math do
   def sum_list([], accumulator) do
     accumulator
   end
+
+  # 1. BASE CASE: Agar list khaali hai, toh sum 0 hai. (Break lag gayi!)
+  def sum([]), do: 0
+
+  # 2. RECURSIVE STEP: Pehla number nikalo aur baqi list ko dobara sum karo.
+  def sum([head | tail]) do
+    head + sum(tail)
+  end
 end
 
-IO.puts Math.sum_list([1, 2, 3], 0) #=> 6
+# => 6
+IO.puts(Math.sum_list([1, 2, 3], 0))
