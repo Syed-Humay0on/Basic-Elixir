77914164d4db2445c7552103ddd8ba4099f4b49c
Proto <Humayoon04syed@gmail.com>
2026-03-10 20:27:43 +0500

    💧ELixir: [IO]

--- lib/IO.ex
+++ lib/IO.ex
@@ -1,12 +1,27 @@
 defmodule IO_Device do
   def input do
-    IO.gets("What is your name?\n") 
+    IO.gets("What is your name?\n")
     |> String.trim()
     |> String.split()
     |> Enum.join()
-  end 
-  def caiptalize do
+  end
+
+  def capitalize do
     IO.gets("Let's capitalize on this shit\n")
+    |> String.capitalize()
+  end
+
+  def checker() do
+    " hello   WORLD! 123   "
+    # dispatch leading/trailing spaces
+    |> String.trim()
+    # lowercase
+    |> String.downcase()
+    # remove numbers (regex)
+    |> String.replace(~r/\d+/, "")
+    # replace ! with ?
+    |> String.replace("!", "?")
+    # First letter capitalize
     |> String.capitalize()
   end
 end
