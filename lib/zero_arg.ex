1b0be1b721a0676957982a4a94e33b62212d9166
Proto <Humayoon04syed@gmail.com>
2026-03-14 23:57:30 +0500

    💧ELixir: [Ex: Zero arg]

--- lib/zero_arg.ex
+++ lib/zero_arg.ex
@@ -1,7 +1,30 @@
-defmodule Greeting do
-  # Ye function zero arguments leta hai → /0
-  def hello() do
-    IO.puts("Assalam-o-Alaikum jani!")
-    :ok   # return value (optional, lekin achi practice)
+defmodule PipeCase2 do
+  def process_response(response) do
+    response
+    |> extract_body()
+    |> handle_body()
   end
+
+  defp extract_body({:ok, %{body: body}}), do: {:ok, body}
+  defp extract_body({:ok, _}), do: {:error, :no_body}
+  defp extract_body({:error, reason}), do: {:error, reason}
+
+  defp handle_body({:ok, body}) do
+    body
+    |> Jason.decode!()
+    |> case do
+      %{"status" => "success", "data" => data} -> {:ok, data}
+      %{"error" => msg} -> {:error, msg}
+    end
+  end
+
+  defp handle_body(error), do: error
+
+  # Ye zero-argument (/0) helper hai testing ke liye
+  def demo() do
+    dummy_response = {:ok, %{body: ~s({"status": "success", "data": "Hello jani"})}}
+    process_response(dummy_response)
+  end
+
+  # baqi extract_body aur handle_body same...
 end
