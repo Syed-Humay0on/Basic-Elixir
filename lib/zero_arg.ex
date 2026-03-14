f195dd8049237216096638b95235813547ecd2a9
Proto <Humayoon04syed@gmail.com>
2026-03-14 23:55:30 +0500

    💧ELixir: [Ex: Zero arg]

---
+++ lib/zero_arg.ex
@@ -0,0 +1,7 @@
+defmodule Greeting do
+  # Ye function zero arguments leta hai → /0
+  def hello() do
+    IO.puts("Assalam-o-Alaikum jani!")
+    :ok   # return value (optional, lekin achi practice)
+  end
+end
